298db51244e12ddfc2dd3efed7e5cc1a156d909b
Simon Prykhodko <semion.ababo@gmail.om>
2017-04-04 19:02:33 +0200

    All the logic without root hash calculation.

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+extern crate digest;
+extern crate generic_array;
+extern crate sha2;
+
+pub mod merkle_tree;
+
+#[cfg(test)]
+mod tests {
+    use generic_array::GenericArray;
+    use merkle_tree::MerkleTree;
+    use sha2::{Digest, Sha256};
+
+    type Hasher = Sha256;
+    type Hash = GenericArray<u8, <Hasher as Digest>::OutputSize>;
+
+    fn hash(str: &str) -> Hash {
+        let mut digest = Hasher::default();
+        digest.input(str.as_bytes());
+        digest.result()
+    }
+
+    #[test]
+    fn sanity() {
+        let blocks = [
+            "Ten little nigger boys went out to dine;",
+            "One choked his little self, and then there were nine.",
+            "Nine little nigger boys sat up very late;",
+            "One overslept himself, and then there were eight.",
+            "Kight little nigger boys travelling in Devon;",
+            "One said he´d stay there, and then there were seven.",
+            "Seven little nigger boys chopping up sticks;",
+            "One chopped himself in half, and then there were six.",
+            "Six little nigger boys playing with a hive;",
+            "A bumble-bee stung one, and then there were five.",
+            "Five little nigger boys going in for law;",
+            "One got in chancery, and then there were four.",
+            "Four little nigger boys going out to sea;",
+            "A red herring swallowed one, and then there were three.",
+            "Three little nigger boys walking in the Zoo;",
+            "A big bear hugged one, and then there were two.",
+            "Two little nigger boys sitting in the sun;",
+            "One got frizzled up, and then there was one.",
+            "One little nigger boy left all alone;",
+            "He went out and hanged himself and then there were None.",
+        ];
+
+        let heights = [
+            1, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6
+        ];
+
+        let mut tree = MerkleTree::<Hasher>::new();
+        assert_eq!(tree.num_blocks(), 0);
+        assert_eq!(tree.height(), 0);
+        assert!(!tree.is_sealed());
+
+        for i in 0..blocks.len() {
+            tree.add_block(hash(blocks[i]));
+            assert_eq!(tree.num_blocks(), i + 1);
+            assert_eq!(tree.height(), heights[i]);
+            assert!(tree.contains_block(hash(blocks[i])));
+        }
+
+        tree.seal();
+        assert!(tree.is_sealed());
+    }
+}
--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,12 +1,59 @@
 use digest::Digest;
+use generic_array::GenericArray;
+use std::collections::BTreeSet;
+
+// To achieve proper efficiency it would be better to divide this functionality
+// into two parts: MerkleTreeGenerator and MerkleTree. The last one should
+// contain a sorted slice instead of expensive BTreeSet.
 
 #[derive(Default)]
 pub struct MerkleTree<T: Digest> {
-    digest: T,
+    // It would be better to use generic_array::GenericArray<u8, T::OutputSize>
+    // instead, but unfortunately it doesn't implement std::cmp::Ord. Let's
+    // omit writing a wrapping type or forking that repo for simplicity.
+    leaves: BTreeSet<Vec<u8>>,
+    // Would be better to use GenericArray<u8, T::OutputSize> instead of T
+    // if it implemented std::default::Default.
+    root: Option<T>,
 }
 
-impl<T: Digest+Default> MerkleTree<T> {
+impl<T: Digest+Default+Clone> MerkleTree<T> {
     pub fn new() -> MerkleTree<T> {
         MerkleTree{ ..Default::default() }
+    }
+
+    pub fn is_sealed(&self) -> bool {
+        !self.root.is_none()
+    }
+
+    pub fn root_hash(&self) -> GenericArray<u8, T::OutputSize> {
+        self.root.as_ref().unwrap().clone().result()
+    }
+
+    pub fn num_blocks(&self) -> usize {
+        self.leaves.len()
+    }
+
+    pub fn height(&self) -> usize {
+        let len = self.leaves.len();
+        match len {
+            0 | 1 => len,
+            _ => ((len - 1) as f32).log2() as usize + 2,
+        }
+    }
+
+    pub fn contains_block(
+        &self, hash: GenericArray<u8, T::OutputSize>) -> bool {
+        let vec:Vec<u8> = hash.iter().cloned().collect();
+        self.leaves.contains(&vec)
+    }
+
+    pub fn add_block(&mut self, hash: GenericArray<u8, T::OutputSize>) {
+        let vec = hash.iter().cloned().collect();
+        self.leaves.insert(vec);
+    }
+
+    pub fn seal(&mut self) {
+
     }
 }
